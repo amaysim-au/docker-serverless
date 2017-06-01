40800493cebbd6ad2da4005d0abfd1c496c316ad
Frederic Lemay <frederic.lemay@amaysim.com.au>
2017-06-01 18:56:13 +1000

    add API Gateway

--- example/apigw/src/handler.js
+++ example/apigw/src/handler.js
@@ -3,5 +3,19 @@
 const greeting = require('./greeting');
 
 module.exports.greet = (event, context, callback) => {
-  return callback(null, greeting.greet());
+  const html = `
+  <html>
+    <body>
+      <h1>${greeting.greet()}</h1>
+    </body>
+  </html>`;
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
+  };
+  return callback(null, response);
 }
